7a647493748ba64269c37bcb82ecd4707514465a
Christopher Vestman <christophervestman@gmail.com>
2024-06-14 12:35:25 +0200

    feat: inital commit after dev on personal computer

---
+++ README.md
@@ -0,0 +1,4 @@
+# extractDTED.sh
+Short script to extract terrain data from Copernicus DTED. 
+
+Developed on personal mac, will need to be edited to work on linux machines.
---
+++ extractDTED.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Extract files into extractedFiles directory, or change accordingly
+for file in tarFiles/*/*; do 
+		tar -xvf ${file} --include '*.dt2' --directory=extractedFiles
+done
+
+# Create the folder structure and mv the files
+for filePath in ./extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
+		# Extract filename and dirname
+		# N57...
+		dirname=$(basename "$filePath" | awk -F '_' '{split($4, a, "."); print a[1]}')
+		# E001...
+		filename=$(basename "$filePath" | awk -F '_' '{split($6, a, "."); print a[1]}')
+		# Create Dir if not exist
+		mkdir -p "./DTED/${dirname}"
+		# Move & rename the file
+		mv "$filePath" "./DTED/${dirname}/${filename}.dt2"
+done
