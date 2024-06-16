0c3bbd7c794ce7a96290d41a387543da6752ee97
Christopher Vestman <christophervestman@gmail.com>
2024-06-16 17:26:44 +0200

    fix: force current folder

--- extractDTED.sh
+++ extractDTED.sh
@@ -2,20 +2,20 @@
 
 # Extract files into extractedFiles directory, or change accordingly
 for file in tarFiles/*/*; do 
-		mkdir -p extractedFiles && tar -xvf ${file} --include '*.dt2' --directory=extractedFiles
+		mkdir -p $PWD/extractedFiles && tar -xvf ${file} --include '*.dt2' --directory=$PWD/extractedFiles
 done
 
 # Create the folder structure and mv the files
-for filePath in ./extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
+for filePath in $PWD/extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
 		# Extract filename and dirname
 		# N57...
 		dirname=$(basename "$filePath" | awk -F '_' '{split($4, a, "."); print a[1]}')
 		# E001...
 		filename=$(basename "$filePath" | awk -F '_' '{split($6, a, "."); print a[1]}')
 		# Create Dir if not exist
-		mkdir -p "./DTED/${dirname}"
+		mkdir -p "$PWD/DTED/${dirname}"
 		# Move & rename the file
-		cp "$filePath" "./DTED/${dirname}/${filename}.dt2"
+		cp "$filePath" "$PWD/DTED/${dirname}/${filename}.dt2"
 done
 
-rm -rf extractedFiles
+rm -rf $PWD/extractedFiles
