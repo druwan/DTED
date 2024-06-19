db17927833f38e5be882f4897692dcbe4b3635d6
Christopher Vestman <christophervestman@gmail.com>
2024-06-19 16:06:03 +0200

    feat: working with bash comp

--- extractDTED.sh
+++ extractDTED.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Extract files into extractedFiles directory, or change accordingly
-for file in tarFiles/*/*; do 
-		mkdir -p $PWD/extractedFiles && tar -xvf ${file} --include '*.dt2' --directory=$PWD/extractedFiles
+# for file in tarFiles/*/*; do 
+#		mkdir -p $PWD/extractedFiles && tar -xvf ${file} --include '*.dt2' --directory=$PWD/extractedFiles
+# done
+
+for file in DEM/*/tarFiles/*; do
+		mkdir -p extractedFiles && tar --directory=extractedFiles -xvf ${file} --wildcards '*.dt2'
 done
+
 
 # Create the folder structure and mv the files
 for filePath in $PWD/extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
@@ -15,7 +20,7 @@ for filePath in $PWD/extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
 		# Create Dir if not exist
 		mkdir -p "$PWD/DTED/${dirname}"
 		# Move & rename the file
-		cp "$filePath" "$PWD/DTED/${dirname}/${filename}.dt2"
+		cp "$filePath" $PWD/terrain/dted_copernicus/dted_copernicus2/dted/${dirname}/${filename}.dt2
 done
 
 rm -rf $PWD/extractedFiles
