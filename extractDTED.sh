348926d03db9d291513724843887cecd01602ccd
Christopher Vestman <christophervestman@gmail.com>
2024-11-04 09:35:41 +0100

    Update extractDTED.sh

--- extractDTED.sh
+++ extractDTED.sh
@@ -5,22 +5,31 @@
 #		mkdir -p $PWD/extractedFiles && tar -xvf ${file} --include '*.dt2' --directory=$PWD/extractedFiles
 # done
 
-for file in DEM/*/tarFiles/*; do
-		mkdir -p extractedFiles && tar --directory=extractedFiles -xvf ${file} --wildcards '*.dt2'
+for country in $PWD/DEM/*; do
+	# Check if country terrain is already extracted
+	if [ $(find "$country" -mindepth 1 -maxdepth 1 -type -d ! -name "tarFiles" | wc -l) -eq 0 ]; then
+		# Extract
+		for tarFile in "$country"/tarFiles/*; do
+			if [ -f "$tarFile" ]; then
+				tar -xvf "$tarFile" -C "$country" --wildcards "*.dt2"
+			fi
+		done
+	else
+		echo -en "$country already extracted"
+	fi
 done
-
 
 # Create the folder structure and mv the files
-for filePath in $PWD/extractedFiles/Copernicus_DSM_10_*/DEM/*.dt2; do
-		# Extract filename and dirname
-		# E001...
-		dirname=$(basename "$filePath" | awk -F '_' '{split($6, a, "."); print a[1]}')
-		# N57...
-		filename=$(basename "$filePath" | awk -F '_' '{split($4, a, "."); print a[1]}')
-		# Create Dir if not exist
-		mkdir -p "$PWD/DTED/${dirname}"
-		# Move & rename the file
-		cp "$filePath" $PWD/terrain/dted_copernicus/dted_copernicus2/dted/${dirname}/${filename}.dt2
+for filePath in $PWD/*/Copernicus_DSM_10_*/DEM/*.dt2; do
+	# Extract filename and dirname
+	# N57...
+	dirname=$(basename "$filePath" | awk -F '_' '{split($6, a, "."); print a[1]}')
+	# E001...
+	filename=$(basename "$filePath" | awk -F '_' '{split($4, a, "."); print a[1]}')
+	# Create Dir if not exist
+	mkdir -p "$PWD/terrain/dted_copernicus/dted_copernicus2/dted/${dirname}"
+	# Copy & rename the file
+        cp "$filePath" $PWD/terrain/dted_copernicus/dted_copernicus2/dted/${dirname}/${filename}.dt2
 done
 
-rm -rf $PWD/extractedFiles
+# TODO: Create the tarFiles Folder struct. Sweden_ -> Remove "_", mv contents into tarFiles
